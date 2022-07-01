3b5e25aaba725aae650af53328e236bc0497d75d
abedddddddd123 <anego237@gmail.com>
2022-07-01 15:08:19 +0700

    UAS_PAW

---
+++ script.js
@@ -0,0 +1,35 @@
+var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.onclick = function() {
+    menu.classList.toggle('active');
+}
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   grabCursor:true,
+   loop:true,
+   autoHeight:true,
+   spaceBetween: 20,
+   breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+   },
+});
